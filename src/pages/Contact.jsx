5b22fe262c6bf9d5fa24e21e149f0e5e606cb934
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-04-23 10:06:29 -0700

    Feat:Setting up the images for my menu page in the website

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,69 @@
-import { Box, Typography} from "@mui/system";
 import React from "react";
 import Layout from "./../components/Layout/Layout";
+import CallIcon from "@mui/icons-material/Call";
+import MailIcon from "@mui/icons-material/Mail";
+import {
+  Box,
+  TableContainer,
+  Typography,
+  TableRow,
+  TableHead,
+  Table,
+  TableCell,
+  Paper,
+  TableBody,
+} from "@mui/material";
 
 const Contact = () => {
   return (
     <Layout>
-      <Box sx={{ my:10, ml:10,"& h4":{fontWeight:'bold',mb:2} }}>
-        <Typography variant="h4">Contact The Restaurant</Typography>
+      <Box sx={{ my: 5, ml: 10, "& h4": { fontWeight: "bold", mb: 2 } }}>
+        <Typography variant="h4">Contact Us</Typography>
         <p>
-        Need to reach us? Call at the number provided or feel free to send us a message through Email! Say hi, tell us how great we're doing, or ask us a question. We will try our best to reply to everyone's messages as soon as we can. 
+          Need to reach us? Call at the number provided or feel free to send a
+          message through Email! Say hi, tell us how great we're doing, or ask
+          us a question. We will try our best to reply to everyone's messages as
+          soon as we can.
         </p>
       </Box>
-
+      <Box
+        sx={{
+          m: 3,
+          width: "600px",
+          ml: 5,
+          "@media (max-width:600px)": {
+            width: "300px",
+          },
+        }}
+      >
+        <TableContainer component={Paper}>
+          <Table aria-label="contact table">
+            <TableHead>
+              <TableRow>
+                <TableCell
+                  sx={{ bgcolor: "black", color: "white" }}
+                  align="center"
+                >
+                  Contact Details
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <CallIcon sx={{ color: "green", pt: 1 }} /> 562.485.2300
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <MailIcon sx={{ color: "skyblue", pt: 1 }} />{" "}
+                  downey@poachedkitchen.com
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Layout>
   );
 };
