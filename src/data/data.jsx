6c7938bf5d68a9d512f24d124f7e0fedfd554b6f
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-04-23 14:52:23 -0700

    Hotfix:Fixed the import statement at the top of the menu page

---
+++ src/data/data.jsx
@@ -0,0 +1,31 @@
+import chilaquiles from '../assets/chilaquiles.jpg';
+import eggsbenedict from '../assets/eggsbenedict.jpg';
+import steakandeggs from '../assets/steakandeggs.jpg';
+import avocadotoast from '../assets/avocadotoast.jpg';
+
+export const MenuList = [
+    {
+        name:'Chilaquiles',
+        description: "The red chilaquiles is made with 2 eggs any style,2 corn tostadas,black beans,chorizo,crema, queso fresco,salsa fresca,cilantro and hashbrowns",
+        image:chilaquiles,
+        price:14
+    },
+    {
+        name:'Eggs Benedict',
+        description: "The eggs benedict are made with 2 poached eggs,English muffin,Beeler ham,serrano aioli,hollandaise,chives,paprika,and hashbrowns",
+        image:eggsbenedict,
+        price:14
+    },
+    {
+        name:'Steak and Eggs',
+        description: "This particular dish is made with 3 eggs any style,6oz petite filet,sautéed mushrooms,grilled onions,herb butter,salsa de tomatillo,hashbrowns and toast",
+        image:steakandeggs,
+        price:29
+    },
+    {
+        name:'Avocado Toast',
+        description: "This dish is made with poached egg,toasted rustic sourdough,avocado,pico de gallo,organic Greek feta,chives and extra virgin olive oil",
+        image:avocadotoast,
+        price:13
+    },
+];
