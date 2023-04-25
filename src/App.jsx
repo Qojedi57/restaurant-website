774c0dd8c14852c4b226b88b4f7790d1bed641e7
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-04-24 22:32:52 -0700

    Chore:added the slight change to my website

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from './pages/Contact';
-import Menu from './pages/Menu';
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import * as React from "react";
 
 function App() {
   return (
-    <div>
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/menu" element={<Menu />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
     </BrowserRouter>
-    </div>
   );
-}
+};
 
 export default App;
